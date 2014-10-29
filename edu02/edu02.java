e3aa66b689797231f0d2e8590995e167b34a6e5a
MakSim345 <senishch@gmail.com>
2014-10-29 08:29:24 +0200

    new test folder for templates

---
+++ edu02/edu02.java
@@ -0,0 +1,10 @@
+public class edu02
+    {
+      public static void main(String[] args)
+      {
+          byte zero = 0;
+          String output = "W" + zero + "W";
+          System.out.println(output);
+          // System.out.println("EDU02 - Hello, World!");
+      }
+    }
