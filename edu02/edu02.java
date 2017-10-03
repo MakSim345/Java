fbc955e74e84ffeeff426ed8b12660b1cd3b2e5f
MakSim345 <senishch@gmail.com>
2017-10-03 10:59:38 +0300

    test SUM in java

--- edu02/edu02.java
+++ edu02/edu02.java
@@ -5,6 +5,21 @@ public class edu02
           byte zero = 0;
           String output = "W" + zero + "W";
           System.out.println(output);
+
+          System.out.println(0123 + 3210);
+
           // System.out.println("EDU02 - Hello, World!");
       }
     }
+
+/*
+public class Sum 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println(0123 + 3210);
+    }
+}
+*/
+
